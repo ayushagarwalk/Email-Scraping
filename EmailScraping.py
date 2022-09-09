999f92abc2bf0e00df2829c84be15c4866989e3d
Shane004 <94621635+Shane004@users.noreply.github.com>
2022-09-10 00:47:29 +0100

    Extended & improved
    takes a website url - searched for all other pages on the domain and then searches for emails with an enhanced regex

--- EmailScraping.py
+++ EmailScraping.py
@@ -1,74 +1,114 @@
 #! python3
 import re, urllib.request, time
+from usp.tree import sitemap_tree_for_homepage
 
-emailRegex = re.compile(r'''
+emailRegex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+') # regex for validating an email format
+"""emailRegex = re.compile(r'''
 #example :
 #something-.+_@somedomain.com
 (
 ([a-zA-Z0-9_.+]+
 @
-[a-zA-Z0-9_.+]+)
+[a-zA-Z0-9_.+]+
+(\.[A-Z|a-z]{2,})+)
 )
-''', re.VERBOSE)
-        
-#Extacting Emails
+''', re.VERBOSE)"""
+allemails = []
+
+# Extacting Emails
 def extractEmailsFromUrlText(urlText):
-    extractedEmail =  emailRegex.findall(urlText)
-    allemails = []
+    emailFile = open("emails.txt", 'a')
+    extractedEmail = emailRegex.findall(urlText)
+    seen = set(allemails)
     for email in extractedEmail:
-        allemails.append(email[0])
-    lenh = len(allemails)
-    print("\tNumber of Emails : %s\n"%lenh )
-    seen = set()
-    for email in allemails:
         if email not in seen:  # faster than `word not in output`
             seen.add(email)
-            emailFile.write(email+"\n")#appending Emails to a filerea
+            emailFile.write(email + "\n")  # appending Emails to a filerea
+            allemails.append(email[0])
+    lenh = len(allemails)
+    print("\tNumber of Emails : %s\n" % lenh)
+    emailFile.close()
 
-#HtmlPage Read Func
+
+# HtmlPage Read Func
 def htmlPageRead(url, i):
     try:
         start = time.time()
-        headers = { 'User-Agent' : 'Mozilla/5.0' }
+        headers = {'User-Agent': 'Mozilla/5.0'}
         request = urllib.request.Request(url, None, headers)
         response = urllib.request.urlopen(request)
         urlHtmlPageRead = response.read()
         urlText = urlHtmlPageRead.decode()
-        print ("%s.%s\tFetched in : %s" % (i, url, (time.time() - start)))
+        print("%s.%s\tFetched in : %s" % (i, url, (time.time() - start)))
         extractEmailsFromUrlText(urlText)
+        print(urlText)
     except:
         pass
-    
-#EmailsLeechFunction
+
+
+# EmailsLeechFunction
 def emailsLeechFunc(url, i):
-    
     try:
-        htmlPageRead(url,i)
+        htmlPageRead(url, i)
     except urllib.error.HTTPError as err:
         if err.code == 404:
             try:
-                url = 'http://webcache.googleusercontent.com/search?q=cache:'+url
+                url = 'http://webcache.googleusercontent.com/search?q=cache:' + url
                 htmlPageRead(url, i)
             except:
                 pass
         else:
-            pass    
-      
-# TODO: Open a file for reading urls
-start = time.time()
-urlFile = open("urls.txt", 'r')
-emailFile = open("emails.txt", 'a')
-i=0
-#Iterate Opened file for getting single url
-for urlLink in urlFile.readlines():
-    urlLink = urlLink.strip('\'"')
-    i=i+1
-    emailsLeechFunc(urlLink, i)
-print ("Elapsed Time: %s" % (time.time() - start))
+            pass
 
-urlFile.close()
-emailFile.close()
+        # TODO: Open a file for reading urls
+
+# Find and Parse Sitemaps to Create List of all website's pages
+from usp.tree import sitemap_tree_for_homepage
 
 
+def getPagesFromSitemap(fullDomain):
+    listPagesRaw = []
+
+    tree = sitemap_tree_for_homepage(fullDomain)
+    for page in tree.all_pages():
+        listPagesRaw.append(page.url)
+
+    return listPagesRaw
 
 
+# Go through List Pages Raw output a list of unique pages links
+def getListUniquePages(listPagesRaw):
+    listPages = []
+
+    for page in listPagesRaw:
+
+        if page in listPages:
+
+            pass
+
+        else:
+
+            listPages.append(page)
+
+    return listPages
+
+def runEmailSearch(listOfUrls):
+    start = time.time()
+    i = 0
+    # Iterate Opened file for getting single url
+    for urlLink in listOfUrls:
+        urlLink = urlLink.strip('\'"')
+        i = i + 1
+        emailsLeechFunc(urlLink, i)
+    print("Elapsed Time: %s" % (time.time() - start))
+
+def runSearch(url):
+    list = getPagesFromSitemap(url)
+    finishedlist = getListUniquePages(list)
+    finishedlist.append(url)
+    runEmailSearch(finishedlist)
+    print(allemails)
+
+
+
+runSearch("https://www.attitudetech.ie")
