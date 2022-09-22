4643168dc91f2e2f57b226375f3c12c8c5b6b954
Shane004 <94621635+Shane004@users.noreply.github.com>
2022-09-22 19:20:21 +0100

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,5 @@
 from googlesearch import search
 from EmailScraping import runEmailSearch
 
-list = search('*"@gmail.com"', num_results=10000000000)
+list = search('strange', num_results=1000)
 runEmailSearch(list)
